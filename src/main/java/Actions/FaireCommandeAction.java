e53bb51768354187a0d2eef92e336de333b6e867
Gheorghe <Gheorghe@Patrick>
2017-05-13 22:36:54 +0200

    1.01

--- src/main/java/Actions/FaireCommandeAction.java
+++ src/main/java/Actions/FaireCommandeAction.java
@@ -10,6 +10,9 @@ import com.google.gson.Gson;
 import com.google.gson.JsonSyntaxException;
 import java.util.List;
 import javax.servlet.http.HttpServletRequest;
+import metier.modele.Client;
+import metier.modele.Commande;
+import metier.modele.Restaurant;
 
 /**
  *
@@ -17,8 +20,9 @@ import javax.servlet.http.HttpServletRequest;
  */
 public class FaireCommandeAction extends Action {
     public class Item{
-        private long id;
-        private String quantity;        
+         long id;
+         String quantity;   
+         
     }    
     public FaireCommandeAction() {
     }
@@ -28,13 +32,15 @@ public class FaireCommandeAction extends Action {
         System.out.println("newCommande");        
         System.out.println(request.getParameter("data"));
         String jsonData=(request.getParameter("data"));        
-        try{
-            Item[] data = new Gson().fromJson(jsonData, Item[].class);
-            System.out.println(data[0]);
-        }catch(JsonSyntaxException e){
-            System.out.println(e);
+        Item[] data = new Gson().fromJson(jsonData, Item[].class);            
+        Commande c= new Commande();
+        c.setClient((Client)request.getSession(false).getAttribute("user"));
+        c.setRestaurant((Restaurant)service.trouverRestaurantParId(Long.parseLong(request.getParameter("id"))));
+        for (Item i:data)
+        {
+            c.AjouterProduit(service.trouverProduitParId(i.id), Integer.parseInt(i.quantity));           
         }
-        
+        service.AttribuerUnLivreurAUneCommandeEtEnregistrer(c);        
     }
     
 }
